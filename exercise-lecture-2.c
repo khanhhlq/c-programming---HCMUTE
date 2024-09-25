13b4884d798b7bbe30b0e7942b7664f054d31067
unknown <lee.quockhanh1107@gmail.com>
2024-09-25 23:29:14 +0700

    done excercise 1

---
+++ exercise-lecture-2.c
@@ -0,0 +1,15 @@
+#include <stdio.h>
+
+int main()
+{
+    int c, thisVariable, g76354, number;
+    printf("Nhap gia tri so nguyen vao bien c: ");
+    scanf("%d", &c);
+    printf("Gia tri so nguyen ma nguoi dung nhap vao la: %d\n", c);
+    if (c != 7) printf("The variable number is not equal to 7\n");
+    printf("This is a C program\n");
+    printf("This is a C\nprogram\n");
+    printf("This\nis\na\nC\nprogram\n");
+    printf("This\tis\ta\tC\tprogram\n");
+    return 0
+}
